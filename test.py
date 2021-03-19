22cc88cb54f2cd15f9281bc791f01c8944298e81
xxchaosxx210 <pmillar1279@googlemail.com>
2021-03-19 10:20:03 +0000

    Initial commit

---
+++ test.py
@@ -0,0 +1,31 @@
+import wx
+from notificationbar import NotificationBar
+import threading
+import time
+
+class TestFrame(wx.Frame):
+
+    def __init__(self):
+        super().__init__(parent=None, id=-1, title="Test Notification")
+
+        pnl = wx.Panel(self, -1)
+        btn = wx.Button(pnl, -1, "Open Notification")
+        btn.Bind(wx.EVT_BUTTON, self._on_notify_btn, btn)
+        gs = wx.GridSizer(cols=1, rows=1, vgap=0, hgap=0)
+        gs.Add(btn, 0, wx.ALIGN_CENTER, 0)
+        pnl.SetSizer(gs)
+        gs = wx.GridSizer(cols=1, rows=1, vgap=0, hgap=0)
+        gs.Add(pnl, 1, wx.EXPAND|wx.ALL, 0)
+        self.SetSizer(gs)
+        self.SetSize(200, 200)
+    
+    def _on_notify_btn(self, evt):
+        def _notify():
+            #time.sleep(3)
+            wx.CallAfter(NotificationBar, self, -1, "", "This is an exmaple text\nto be displayed on\nthe Screen at any onetime")
+        threading.Thread(target=_notify).start()
+
+if __name__ == '__main__':
+    app = wx.App()
+    TestFrame().Show()
+    app.MainLoop()
